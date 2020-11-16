ade76052c73159b774fa22326a2fa7dda5b24368
Eric Yarbrough <eric.yarbrough@utexas.edu>
2020-11-16 12:17:35 -0600

    finalize color palette

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,7 +9,7 @@ const About = () => {
       <div className="row align-items-center text-center">
         <div className="py-1">
           <h2 className="about">
-            <span className="secondary">Who</span> <span>I</span> <span className="pop">Am.</span>
+            Who I Am.
           </h2>
           <p className="text-break text-center m-3">
             My name is Eric Yarbrough! I went to the University of Texas at Austin and graduated in May of 2020 with a B.A. in Anthropology. I know I know, a liberal arts major, but I’ve seen the light, and now I’m addicted to coding. Covid-19 has kept my face glued to the computer. I'm always working or keeping busy and definitely laughing. When I'm not working I enjoy playing competitive F.P.S. games on my PC. Right now, I'm addicted to Valorant and main Cypher and Sova. Also, I love reading fantasy or horror books. I'm trying to tackle George R.R. Martin's series "A Song of Ice and Fire". As for the horror side of things, I'm obsessed with Stephen King's "It". Similarly, I'm a huge movie buff and adore cinematography and analyzing films.
@@ -19,7 +19,7 @@ const About = () => {
       <div className="row align-items-center text-center">
         <div className="py-1">
           <h2 className="about">
-            <span className="pop">What</span> <span>I</span> <span className="secondary">Do.</span>
+            What I Do.
           </h2>
           <p className="text-break m-3">
             I lean towards the back-end of code and find comfort in manipulating data. I can work with both SQL and NoSQL databases and follow the M.V.C. paradigm to give my code structure.  However, I do love a good color palette and find myself mentally redesigning websites. My style is minimal with a touch of playfulness.
