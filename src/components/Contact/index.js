2a36259f005d7ce5d070ebde0c02b62f80a4020b
Eric Yarbrough <eric.yarbrough@utexas.edu>
2020-11-16 14:00:50 -0600

    resize contact form and style for mobile

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { validateEmail } from '../../utils/helpers';
-// import face from '../../assets/face.png';
 
 function ContactForm() {
 
@@ -38,40 +37,35 @@ function ContactForm() {
     }
 
     return (
-        <section className='container-fluid'>
-            <div className="align-items-center py-3">
-                <div className=" d-flex justify-content-center">
-                    <div className="  justify-content-center text-center">
+        <section>
+            <div className=" d-flex justify-content-center">
+                <div className="justify-content-center">
+                    <div className="text-center">
                         <p>(Currently Under development)</p>
                         <h2 className="p-3">Contact Me</h2>
-
-                        <p>Email: <a href='mailto: ea.yarbrough98@gmail.com' alt="Email">ea.yarbrough98@gmail.com</a></p>
-
-                        {/* <form id="contact-form" onSubmit={handleSubmit}>
-                            <div className="p-3">
-                                <label htmlFor="name">Full Name:</label>
-                                <input type="text" name="Full Name" onBlur={handleChange} defaultValue={name} />
-                            </div>
-                            <div className="p-3">
-                                <label htmlFor="email">Email address:</label>
-                                <input type="email" name="email" onBlur={handleChange} defaultValue={email} />
-                            </div>
-                            <div className="p-3">
-                                <label htmlFor="essage">Message:</label>
-                                <textarea name="Message" rows="5" onBlur={handleChange} defaultValue={message} />
-                            </div>
-                            {errorMessage && (
-                                <div className="p-3">
-                                    <p className="error-text">{errorMessage}</p>
-                                </div>
-                            )}
-                            <button type="submit" className="m-3">Submit</button>
-                        </form> */}
                     </div>
+
+                    <form id="contact-form" onSubmit={handleSubmit}>
+                        <div>
+                            <label htmlFor="name">Full Name:</label>
+                            <input type="text" name="Full Name" onBlur={handleChange} defaultValue={name} />
+                        </div>
+                        <div>
+                            <label htmlFor="email">Email address:</label>
+                            <input type="email" name="email" onBlur={handleChange} defaultValue={email} />
+                        </div>
+                        <div>
+                            <label htmlFor="essage">Message:</label>
+                            <textarea name="Message" rows="5" onBlur={handleChange} defaultValue={message} />
+                        </div>
+                        {errorMessage && (
+                            <div>
+                                <p className="error-text">{errorMessage}</p>
+                            </div>
+                        )}
+                        <button type="submit" className="m-3">Send Message</button>
+                    </form>
                 </div>
-                {/* <div className="col-lg-6 d-flex justify-content-center">
-                    <img src={face} alt="art with head falling off"></img>
-                </div> */}
             </div>
         </section>
     )
