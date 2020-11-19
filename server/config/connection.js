ee232c4545ccd12e23e5c1b09c4b91b3af243bcb
Eric Yarbrough <eric.yarbrough@utexas.edu>
2020-11-19 09:43:37 -0600

    edit name of database

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/portfolio', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/react-portfolio', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
