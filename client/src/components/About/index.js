8a361f3d37fe2d2e776d1202964967c54d3977cd
Eric Yarbrough <eric.yarbrough@utexas.edu>
2020-12-15 03:25:21 -0600

    edit about format and add more text

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -6,24 +6,37 @@ import React from 'react';
 const About = () => {
   return (
     <div className="container-fluid py-3">
-      <div className="row align-items-center text-center">
+      <div className="row align-items-center ">
         <div className="py-1">
-          <h2 className="about">
+          <h2 className="about  text-center">
             Who I Am.
           </h2>
-          <p className="text-break text-center m-3">
-            My name is Eric Yarbrough! I went to the University of Texas at Austin and graduated in May of 2020 with a B.A. in Anthropology. I know I know, a liberal arts major, but I’ve seen the light, and now I’m addicted to coding. Covid-19 has kept my face glued to the computer. I'm always working or keeping busy and definitely laughing. When I'm not working I enjoy playing competitive F.P.S. games on my PC. Right now, I'm addicted to Valorant and main Cypher and Sova. Also, I love reading fantasy or horror books. I'm trying to tackle George R.R. Martin's series "A Song of Ice and Fire". As for the horror side of things, I'm obsessed with Stephen King's "It". Similarly, I'm a huge movie buff and adore cinematography and analyzing films.
-            </p>
+          <p className="text-break m-3">
+            My name is Eric Yarbrough! I went to the University of Texas at Austin and graduated in May of 2020 with a B.A. in Anthropology. I know I know, a liberal arts major, but I’ve seen the light, and now I’m addicted to coding. Covid-19 has kept my face glued to the computer. I'm always working or keeping busy and definitely laughing.
+          </p>
+          <p className="text-break m-3">
+            When I'm not working I enjoy playing competitive F.P.S. games on my PC. I'm addicted to Valorant and main Cypher and Sova.
+          </p>
+          <p className="text-break m-3">
+            I love reading fantasy or horror books. I'm trying to tackle George R.R. Martin's series "A Song of Ice and Fire". Star Wars: The Clone Wars is by far one of my favorite series, that along with Avatar: The Last Airbender. My favorite characters include Ashoka Tano, Uncle Iroh, and Azula.
+          </p>
+
+          <p className="text-break m-3">
+            Family is monumental to me, I wouldn’t be where I am in life without the support of my loved ones.
+          </p>
         </div>
       </div>
-      <div className="row align-items-center text-center">
+      <div className="row align-items-center ">
         <div className="py-1">
-          <h2 className="about">
+          <h2 className="about text-center">
             What I Do.
           </h2>
           <p className="text-break m-3">
             I lean towards the back-end of code and find comfort in manipulating data. I can work with both SQL and NoSQL databases and follow the M.V.C. paradigm to give my code structure.  However, I do love a good color palette and find myself mentally redesigning websites. My style is minimal with a touch of playfulness.
-            </p>
+          </p>
+          <p className="text-break m-3">
+            I tend to work closely with people and will regularly touch base and relay ideas that might come--up. I thoroughly enjoy the brainstorming process and planning out code in my notebook (its full of rough sketches and terrible ideas).
+          </p>
         </div>
         <div>
         </div>
