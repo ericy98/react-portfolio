7fa263de0ec9fec802e7bae99af6c3dc8d8aec71
Eric Yarbrough <eric.yarbrough@utexas.edu>
2021-02-01 11:38:58 -0600

    design homepage img

--- client/src/components/Homepage/index.js
+++ client/src/components/Homepage/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 
+
 function Home() {
     return (
-        <div className="text-center py-2 homeBG">
+        <div className="text-center py-2 ">
             <div>
-                <h1 className="head1">Eric.</h1>
+                <img src={require(`../../assets/homeBG.png`)} />
+                
             </div>
-            <div>
-                <h1 className="head2">Full-Stack Web Developer.</h1>
-            </div>
-            <div>
-                <h1 className="head1">Austin, Texas.</h1>
-            </div>
+
         </div>
     )
 }
