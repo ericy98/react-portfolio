76466d817f79fa4f1f02cc49b2a4e6e4e0ee432f
Eric Yarbrough <eric.yarbrough@utexas.edu>
2021-02-02 19:14:00 -0600

    change homeimg when in mobile

--- client/src/components/Homepage/index.js
+++ client/src/components/Homepage/index.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import desktopImg from '../../assets/homeBG.png';
+import mobileImg from '../../assets/homeMobileBG.png';
 
 
 function Home() {
+
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    const imageUrl = windowWidth >= 750 ? desktopImg : mobileImg;
+
+    useEffect (() => {
+        const handleWindowResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+
+        window.addEventListener('resize', handleWindowResize);
+
+        return () => {
+            window.removeEventListener('resize', handleWindowResize);
+        }
+
+    }, []);
+
     return (
         <div className="text-center py-2 ">
             <div>
-                <img src={require(`../../assets/homeBG.png`)} />
+                <img className="homeImg" src={(`${imageUrl}`)} alt="Eric. Full-Stack Web Developer. Austin, TX" />
                 
             </div>
 
